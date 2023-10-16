65bb662386c5a0e333a5662adea089bbfccad1c8
Max Zviagintsev <maxim.zviagintsev@gmail.com>
2023-10-16 14:58:40 +0300

    Add fonts 2
    fix TS errors

--- src/App.tsx
+++ src/App.tsx
@@ -1,42 +1,12 @@
-import { useState } from "react";
+import React from "react";
 import styled from "styled-components";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
 
 const AwesomeHeadline = styled.h1`
   font-family: "Ailerons-Typeface";
 `;
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-
-      <AwesomeHeadline>Vite + React</AwesomeHeadline>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-
-        <p>
-          Edit <code>src/App.tsx</code> Витя жопа жирная
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  );
+  return <AwesomeHeadline>Vite + React</AwesomeHeadline>;
 }
 
 export default App;
--- src/fonts/fonts.ts
+++ src/fonts/fonts.ts
@@ -8,55 +8,47 @@ import Exo2BoldItalicTTF from "./exo-20-bold-italic.ttf";
 import Exo2ItalicTTF from "./exo-20-italic.ttf";
 
 const GlobalFonts = createGlobalStyle`
-{ 
-    {  
         @font-face {
         font-family: "Elianto-Regular";
         font-style: normal;
         font-weight: normal;
         src: url(${EliantoRegularTTF}) format("truetype");
-        }
-    },
-    { 
+        };
+
         @font-face {
         font-family: "Ailerons-Typeface";
         font-style: normal;
         font-weight: normal;
         src: url(${AileronsTypefaceOTF}) format("opentype");
-        }
-    },
-    { 
+        };
+
         @font-face {
         font-family: "Exo 2.0";
         font-style: normal;
         font-weight: normal;
         src: url(${Exo2TTF}) format("truetype");
-        }
-    },
-    { 
+        };
+
         @font-face {
         font-family: "Exo 2.0, italic";
         font-style: normal;
         font-weight: normal;
         src: url(${Exo2ItalicTTF}) format("truetype");
-        }
-    },
-        { 
+        };
+        
         @font-face {
         font-family: "Exo 2.0, bold";
         font-style: normal;
         font-weight: normal;
         src: url(${Exo2BoldTTF}) format("truetype");
-        }
-    },
-        { 
+        };
+
         @font-face {
         font-family: "Exo 2.0, bold italic";
         font-style: normal;
         font-weight: normal;
         src: url(${Exo2BoldItalicTTF}) format("truetype");
-        }
-    }
-}`;
+        };
+`;
 
 export default GlobalFonts;
